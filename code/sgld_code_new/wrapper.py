111ef01c321f0a5c074935e625a748d04ff6d958
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-04-20 20:26:50 -0400

    Working SGLD (reasonably)

--- code/sgld_code_new/wrapper.py
+++ code/sgld_code_new/wrapper.py
@@ -29,21 +29,23 @@ class Langevin_Wrapper():
         self.optimizer = SGLD(self.network.parameters(),
                               lr=self.learn_rate,
                               num_pseudo_batches=self.no_batches,
-                              num_burn_in_steps=num_burn_in_steps)
+                              num_burn_in_steps=num_burn_in_steps,
+                              precondition_decay_rate=0.95)
+
         # self.optimizer = torch.optim.SGD(
         #     self.network.parameters(), lr=self.learn_rate
         # )
-        # self.loss_func = torch.nn.MSELoss(reduction='mean')
-        self.loss_func = sgld_utils.log_gaussian_loss
+        self.loss_func = torch.nn.MSELoss(reduction='mean')
+        # self.loss_func = sgld_utils.log_gaussian_loss
 
-        self.scheduler = torch.optim.lr_scheduler.StepLR(
-            self.optimizer, step_size=int(num_epochs//6), gamma=0.5)  # to reduce by 100 over the course of optimization
+        # self.scheduler = torch.optim.lr_scheduler.StepLR(
+        #     self.optimizer, step_size=int(num_epochs//6), gamma=0.5)  # to reduce by 100 over the course of optimization
 
     def fit(self, x, y):
         # x, y = sgld_utils.to_variable(var=(x, y), cuda=self.cuda)
 
         output = self.network(x)
-        # loss = self.loss_func(output, y)
+        loss = self.loss_func(output, y)
 
         # # compute the prior on parameters:
         # prior = 0.0
@@ -51,14 +53,14 @@ class Langevin_Wrapper():
         #     if param.requires_grad:
         #         prior += (0.5)*torch.sum(torch.pow(param, 2))
 
-        loss = self.loss_func(
-            output, y, self.network.noise, 1)  # + 0.0*prior
+        # loss = 0.5*self.loss_func(
+        #     output, y, self.network.noise, 1)  # + 0.0*prior
 
         # reset gradient and total loss
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-        self.scheduler.step()
+        # self.scheduler.step()
 
         return loss
 
